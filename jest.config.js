3fc319ece7263f7e62e861b38fbe917e4bca8fa2
Bankk <106103625+BankkRoll@users.noreply.github.com>
2024-05-29 14:45:56 -0400

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jest-environment-jsdom',
-    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-    transform: {
-        "^.+\\.jsx?$": "babel-jest",
-        "^.+\\.tsx?$": "ts-jest",
-      },
-    };
